fec3e4e2822dec38257754f0e7a07e9b333de332
Sundar Rajan Muthuraj <msundarrajan@gmail.com>
2023-08-27 12:36:51 +0530

    Add /api/time

--- app/app.go
+++ app/app.go
@@ -16,6 +16,7 @@ func Start() {
 	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
 	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer)
+	router.HandleFunc("/api/time", getTime)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
--- app/handlers.go
+++ app/handlers.go
@@ -3,8 +3,11 @@ package app
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -17,6 +20,51 @@ type Customer struct {
 	Name    string `json:"full_name" xml:"name"`
 	City    string `json:"city" xml:"city"`
 	Zipcode string `json:"zip_code" xml:"zipcode"`
+}
+
+func GetTimeFromTimezone(tz string) (string, error) {
+	loc, err := time.LoadLocation(tz)
+	if err != nil {
+		return "", errors.New("invalid timezone")
+	}
+	return time.Now().In(loc).String(), nil
+}
+
+func handleTimezone(timezone string, w http.ResponseWriter) {
+	response := map[string]string{}
+	timezones := strings.Split(timezone, ",")
+
+	for i := 0; i < len(timezones); i++ {
+		tz := timezones[i]
+
+		loc, err := GetTimeFromTimezone(strings.TrimSpace(tz))
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, err.Error())
+			return
+		}
+
+		response[tz] = loc
+	}
+
+	w.Header().Add("content-type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func handleUTC(w http.ResponseWriter) {
+	w.Header().Add("content-type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"current_time": time.Now().UTC().String(),
+	})
+}
+func getTime(w http.ResponseWriter, r *http.Request) {
+	timezone := r.URL.Query().Get("tz")
+
+	if timezone != "" {
+		handleTimezone(timezone, w)
+	} else {
+		handleUTC(w)
+	}
 }
 
 func getAllCustomers(w http.ResponseWriter, r *http.Request) {
